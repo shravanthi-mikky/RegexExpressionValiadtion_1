4f4fc6463be4534c73983931b2df0451123f9a95
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-05-16 16:52:22 +0530

    [shravanthi] - validation for FirstName

---
+++ Regex1/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Regex1;
+
+Console.WriteLine("Validation for Form ");
+RegexValidation r1 = new();
+r1.FirstNameValidation();
---
+++ Regex1/Regex.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Regex1
+{
+    public class RegexValidation
+    {
+        public void FirstNameValidation()
+        {  
+            Console.WriteLine("Enter First Name");
+            string Firstname = Console.ReadLine();
+            string firstname = "^[A-Z]{1}[a-z]{2,}";
+            Regex regex = new Regex(firstname);
+
+            if (regex.IsMatch(Firstname))
+            {
+                Console.Write("Name is Valid \n");
+            }
+            else
+            {
+                Console.Write("Please Enter First Letter Capital \n");
+            }
+        }
+    }
+}
